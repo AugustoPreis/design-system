cf383d2e239100ff49cde50ee5961b7540fbbea2
Augusto Tomasi <augusto.tomasi@innspire.dev>
2026-06-11 17:13:27 -0300

    feat(ui): add Image component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -9,6 +9,7 @@ export { Stack, type StackProps } from './components/stack';
 export { Badge, type BadgeProps, badgeVariants } from './components/badge';
 export { Button, type ButtonProps, buttonVariants } from './components/button';
 export { Heading, type HeadingProps } from './components/heading';
+export { Image, type ImageProps } from './components/image';
 export { Skeleton, type SkeletonProps } from './components/skeleton';
 export { Spinner, type SpinnerProps } from './components/spinner';
 export { Text, type TextProps } from './components/text';
